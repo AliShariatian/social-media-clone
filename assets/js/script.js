491582fe3521c05e93e0cedb0c6fdde335b700af
Ali Shariatian <shariatian92@gmail.com>
2023-11-25 14:20:55 +0330

    add: handle notif box and count

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
-// Input create post box shadow effect
 const createPostInput = document.getElementById("create-post");
 const createPostContainer = document.getElementById("create-post-container");
 
+const menuItems = document.querySelectorAll(".menu-item");
+const notificationsPopup = document.querySelector(".notifications-popup");
+const noticicationsCount = document.querySelector("#notification .notification-count");
+
+// Input create post box shadow effect
 createPostInput.addEventListener("focusin", () => {
     createPostContainer.classList.remove("border-color");
     createPostContainer.classList.add("shadow");
@@ -12,3 +16,24 @@ createPostInput.addEventListener("focusout", () => {
     createPostContainer.classList.remove("shadow");
 });
 
+// Sidebar
+// remove active class from all menu items
+const changeActiveItem = () => {
+    menuItems.forEach((item) => {
+        item.classList.remove("active");
+    });
+};
+
+menuItems.forEach((item) => {
+    item.addEventListener("click", () => {
+        changeActiveItem();
+        item.classList.add("active");
+
+        if (item.id != "notification") {
+            notificationsPopup.style.display = "none";
+        } else {
+            notificationsPopup.style.display = "block";
+            noticicationsCount.style.display = "none";
+        }
+    });
+});
